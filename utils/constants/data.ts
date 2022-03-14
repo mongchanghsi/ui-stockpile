802427d5b583bb2c7558f12899bd25f862b2d5de
mongchanghsi <m.changhsi@gmail.com>
2022-03-15 00:02:06 +0800

    Add dummy data

---
+++ utils/constants/data.ts
@@ -0,0 +1,27 @@
+export const FiveItemLists = [
+  {
+    number: 1,
+    title: 'Block 1',
+    content: 'Content 1',
+  },
+  {
+    number: 2,
+    title: 'Block 2',
+    content: 'Content 2',
+  },
+  {
+    number: 3,
+    title: 'Block 3',
+    content: 'Content 3',
+  },
+  {
+    number: 4,
+    title: 'Block 4',
+    content: 'Content 4',
+  },
+  {
+    number: 5,
+    title: 'Block 5',
+    content: 'Content 5',
+  },
+];
