c1b8033b0fbf3e2e72067856e289cdcfb1b4e2b1
mongchanghsi <m.changhsi@gmail.com>
2022-03-15 08:25:58 +0800

    Add detailed styling of azuki grid

--- components/AzukiGridDisplay/GridItem/index.tsx
+++ components/AzukiGridDisplay/GridItem/index.tsx
@@ -1,9 +1,16 @@
+import { IFiveItemList } from '../../../utils/constants/data';
 import styles from './index.module.scss';
 
-const GridItem = ({ data }: { data: any }) => {
+const GridItem = ({ data }: { data: IFiveItemList }) => {
   return (
     <div className={styles.main}>
-      <p>{data.item}</p>
+      <div>
+        <p className={styles.content_text}>{data.content}</p>
+      </div>
+      <div>
+        <h3 className={styles.content_number}>0{data.number}</h3>
+        <h4 className={styles.content_title}>{data.title}</h4>
+      </div>
     </div>
   );
 };
