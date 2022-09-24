4845d87fa30d516edc41013a74e76fcbb395f9a1
laiyaohao <laiyaohao96@gmail.com>
2022-09-25 00:02:33 +0800

    fix: reverted code changes

--- components/ProximityHover/ProximityTitle/index.tsx
+++ components/ProximityHover/ProximityTitle/index.tsx
@@ -6,17 +6,14 @@ import { IMovement } from "..";
 
 const ProximityTitle = ({ data }: { data: IMovement }) => {
   return (
-    <div className={styles.container}>
-      <Image
-        src={RickAndMortyTitle}
-        alt="Rick and Morty Title"
-        layout="fill"
-        style={{
-          transform: `translate(-${data.x}px, -${data.y}px)`,
-        }}
-      />
+    <div
+      className={styles.container}
+      style={{
+        transform: `translate(-${data.x}px, -${data.y}px)`,
+      }}
+    >
+      <Image src={RickAndMortyTitle} alt="Rick and Morty Title" layout="fill" />
     </div>
-  );
 };
 
 export default ProximityTitle;
